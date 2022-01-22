6bbe9f79b5e404ea6eac1238ba75d1b4cd2dc9f5
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-01-22 17:23:08 +0900

    4 commit

--- 가장_큰_수.py
+++ 가장_큰_수.py
@@ -4,14 +4,7 @@ numbers = [0, 0, 0]
 
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numSort = sorted(numbers, key=lambda x: x[3]if len(
-        x) >= 4 else x[0], reverse=True)
-    numSort = sorted(numSort, key=lambda x: x[2]if len(
-        x) == 3 else ('0' if len(x) > 3 else x[0]), reverse=True)
-    numSort = sorted(numSort, key=lambda x: x[1]if len(
-        x) == 2 else ('0' if len(x) > 2 else x[0]), reverse=True)
-    numSort = sorted(numSort, key=lambda x: x[0], reverse=True)
-
+    numSort = sorted(numbers, key=lambda x: x*3, reverse=True)
     answer = "".join(numSort)
     if answer[0] == '0':
         answer = '0'
