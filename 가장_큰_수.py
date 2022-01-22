cd89e380e55f2c7ba62acd3b908d445da9ce5fb8
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2022-01-22 17:05:10 +0900

    second commit

--- 가장_큰_수.py
+++ 가장_큰_수.py
@@ -1,14 +1,15 @@
 numbers = [979, 97, 978, 81, 818, 817]
+# 979 97 978 818 81 817
 
 
 def solution(numbers):
     numbers = list(map(str, numbers))
     numSort = sorted(numbers, key=lambda x: x[3]if len(
-        x) >= 4 else (x[2] if len(x) >= 3 else '0'), reverse=True)
+        x) >= 4 else x[0], reverse=True)
     numSort = sorted(numSort, key=lambda x: x[2]if len(
-        x) >= 3 else (x[1] if len(x) >= 2 else '0'), reverse=True)
+        x) == 3 else ('0' if len(x) > 3 else x[0]), reverse=True)
     numSort = sorted(numSort, key=lambda x: x[1]if len(
-        x) >= 2 else x[0], reverse=True)
+        x) >= 2 else ('0' if len(x) > 2 else x[0]), reverse=True)
     numSort = sorted(numSort, key=lambda x: x[0], reverse=True)
 
     answer = "".join(numSort)
